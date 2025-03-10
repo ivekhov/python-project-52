899fd62bcc9d2415660449ea42880d9bb15fd098
Ivan Vekhov <ivekhov@gmail.com>
2025-03-10 08:52:45 +0300

    add auth step

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,16 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from task_manager import views
+from .views import IndexView
+
 
 urlpatterns = [
-    path('', views.index),
+    path('', IndexView.as_view()),
     path('users/', include('task_manager.users.urls'), name='users'),
     path('login/', views.login, name='login'),
     path('registration/', views.registration, name='registration'),
     path('admin/', admin.site.urls, name='admin'),
-
-
-    # routes from task
-    # path(),
-
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
-
-
-# TODO
-def users(request):
-    return render(request)
+class IndexView(View):
+    '''.'''
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'index.html'
+        )
 
 
 def login(request):
